141fc1faca5e7b16fe3990f044d1ea76c2cd4b73
Anatoliy <anatoliy.rus@gmail.com>
2018-08-02 22:24:02 +0300

    Добавление новых продуктов

--- lesson_5/js/app.js
+++ lesson_5/js/app.js
@@ -1,188 +1,293 @@
 $(function () {
-    // globalEvents = _.extend({}, Backbone.Events);
-    Product = Backbone.Model.extend({
-        defaults: {
-            title: 'none',
-            level: 0,
-            productivity: 0.1,
-            complete: 0,
-            consumes: null
+  // globalEvents = _.extend({}, Backbone.Events);
+  Product = Backbone.Model.extend({
+    defaults: {
+      title: 'none',
+      level: 0,
+      productivity: 0.1,
+      complete: 0,
+      consumes: null
+    }
+  });
+
+  Farm = Backbone.Collection.extend({
+    model: Product
+  });
+
+  StoreItem = Backbone.Model.extend({
+    defaults: {
+      title: 'none',
+      amount: 0,
+      price: 0
+    }
+  });
+
+  Store = Backbone.Collection.extend({});
+
+  ProductView = Backbone.View.extend({
+    tagName: 'td',
+    template_render: _.template($('#productView_template').html()),
+    template_update: _.template($($('#productView_template').html()).find('#productView_template_update').html()
+      .replace(/\&lt;/g, '<').replace(/\&gt;/g, '>')),
+
+    /*
+        template_update: _.template('<div class="level"><%= level %></div>\n' +
+          '                <div class="production"><%= complete %>%</div>'),
+    */
+    initialize: function () {
+      var thisView = this;
+      // this.$el = $('#' + this.model.get('title'));
+      _.bindAll(thisView, 'render');
+      this.listenTo(this.model, 'completeProduction', this.completeProduction);
+      this.listenTo(this.model, 'upgrade', this.upgrade);
+      this.model.bind("all", function () {
+        thisView.update();
+      });
+    },
+
+    upgrade: function () {
+      var level = this.model.get('level');
+      this.model.set('level', level + 1);
+    },
+    completeProduction: function () {
+      var storedProducts = myStore.where({title: this.model.get('title')});
+      var amount = storedProducts[0].get('amount');
+      var complete = this.model.get('complete');
+      var produced = Math.floor(this.model.get('complete') / 100);
+      this.model.set('complete', Math.round(complete / 100 - produced) * 100);
+      storedProducts[0].set('amount', amount + produced);
+      if (this.model.get('consumes') !== null) {
+        storedProducts = myStore.where({title: this.model.get('consumes')});
+        if (storedProducts.length == 0 || storedProducts[0].get('amount') < 10) {
+          return;
         }
-    });
+        var amount = storedProducts[0].get('amount');
+        storedProducts[0].set('amount', amount - 10);
+      }
+      if (Math.random() > 0.5) {
+        this.model.trigger('upgrade');
+      }
+    },
 
-    Farm = Backbone.Collection.extend({
-        model: Product
-    });
+    update: function () {
+      this.$el.find('.data').html(this.template_update(this.model.attributes));
+    },
 
-    StoreItem = Backbone.Model.extend({
-        defaults: {
-            title: 'none',
-            amount: 0,
-            price: 0
-        }
-    });
+    render: function () {
+      this.$el.html(this.template_render(this.model.attributes));
+      return this;
+    }
+  });
 
-    Store = Backbone.Collection.extend({});
+  ProductViewCollectoin = Backbone.View.extend({
+    tagName: 'tr',
+    $parentTable: $('#farm'),
+    initialize: function () {
+      // this.collection.on('add', this.test(), this);
+      this.collection.on('add', this.renderOne, this);
+      this.render();
+    },
 
-    ProductView = Backbone.View.extend({
-        tagName: 'div',
-        template: _.template('<div class="level"><%= level %></div>\n' +
-            '                <div class="production"><%= complete %>%</div>'),
-        initialize: function () {
-            var thisView = this;
-            this.$el = $('#' + this.model.get('title'));
-            _.bindAll(thisView, 'render');
-            this.listenTo(this.model, 'completeProduction', this.completeProduction);
-            this.listenTo(this.model, 'upgrade', this.upgrade);
-            this.model.bind("all", function (model) {
-                thisView.render(model);
-            });
-        },
-        upgrade: function () {
-            var level = this.model.get('level');
-            this.model.set('level', level + 1);
-        },
-        completeProduction: function () {
-            var storedProducts = myStore.where({title: this.model.get('title')});
-            var amount = storedProducts[0].get('amount');
-            var complete = this.model.get('complete');
-            var produced = Math.floor(this.model.get('complete') / 100);
-            this.model.set('complete', Math.round(complete/100 - produced)*100);
-            storedProducts[0].set('amount', amount + produced);
-            if (this.model.get('consumes') !== null) {
-                storedProducts = myStore.where({title: this.model.get('consumes')});
-                if (storedProducts.length == 0 || storedProducts[0].get('amount') < 10) {
-                    return;
-                }
-                var amount = storedProducts[0].get('amount');
-                storedProducts[0].set('amount', amount - 10);
-            }
-            if (Math.random() > 0.5) {
-                this.model.trigger('upgrade');
-            }
-        },
-        render: function (model) {
-            this.$el.find('.data').html(this.template(this.model.attributes));
-            return this;
-        }
-    });
+    render: function () {
+      this.collection.each(function (model) {
+        this.renderOne(model);
+      }, this);
+      this.$parentTable.append(this.el);
+    },
 
-    var storeViews = {};
+    renderOne: function (model) {
+      this.$el.append(new ProductView({model: model}).render().el);
+    }
+  });
 
-    storeViews.storeItem = Backbone.View.extend({
-        tagName: 'tr',
-        template: _.template('<td><%= title %></td>\n' +
-            '                 <td><%= price %></td>\n' +
-            '                 <td><%= amount %></td>\n' +
-            '                 <td><%- price * amount %></td>'),
-        initialize: function (options) {
-            _.bindAll(this, 'render');
-            this.model.bind('change', this.render);
-        },
+  var storeViews = {};
 
-        render: function () {
-            this.$el.empty();
-            this.$el.html(this.template(this.model.attributes));
-            return this;
-        }
-    });
+  storeViews.storeItem = Backbone.View.extend({
+    tagName: 'tr',
+    template: _.template('<td><%= title %></td>\n' +
+      '                 <td><%= price %></td>\n' +
+      '                 <td><%= amount %></td>\n' +
+      '                 <td><%- price * amount %></td>'),
+    initialize: function (options) {
+      _.bindAll(this, 'render');
+      this.model.bind('change', this.render);
+      this.model.on('fire', this.fire, this);
+      this.model.on('wildChickens', this.wildChickens, this);
+    },
 
-    storeViews.Store = Backbone.View.extend({
-        collection: null,
-        initialize: function () {
-            _.bindAll(this, 'render');
-            this.collection.bind('all', this.render);
-        },
+    wildChickens: function () {
+      var amountWildchickens = _.random(0, 20);
+      console.log('amountWildchickens: ', amountWildchickens, myStore);
+      myStore.where({title: 'hen'})[0].set('amount',
+        myStore.where({title: 'hen'})[0].get('amount') + amountWildchickens);
+    },
 
-        render: function () {
-            var element = this.$el;
-            element.empty();
+    fire: function () {
+      console.log('fff', this.model.toJSON());
+      this.model.set('amount', 0);
+    },
 
-            this.collection.forEach(function (item) {
+    render: function () {
+      this.$el.empty();
+      this.$el.html(this.template(this.model.attributes));
+      return this;
+    }
+  });
+
+  storeViews.Store = Backbone.View.extend({
+    collection: null,
+    initialize: function () {
+      _.bindAll(this, 'render', 'renderOne');
+      this.collection.on('add', this.renderOne, this);
+      // this.collection.bind('all', this.render);
+    },
+
+    render: function () {
+      var element = this.$el;
+      element.empty();
+
+      this.collection.forEach(function (model) {
+        this.renderOne(model);
+        /*
                 var itemView = new storeViews.storeItem({
-                    model: item
+                  model: model
                 });
+
                 element.append(itemView.render().el);
-            });
+              */
+      }, this);
+      return this;
+    },
 
-            return this;
-        }
-    });
+    renderOne: function (model) {
+      this.$el.append(new storeViews.storeItem({model: model}).render().el);
+    }
+  });
 
-    myfarm = new Farm;
-    myStore = new Store;
-    farmSetup = [
-        {
-            title: 'hen',
-            productivity: 1,
-            level: 1,
-            consumes: 'grain'
-        },
-        {
-            title: 'fox',
-            productivity: 0.5,
-            level: 1,
-            consumes: 'hen'
-        },
-        {
-            title: 'grain',
-            productivity: 5,
-            level: 1,
-            consumes: null
-        }
-    ];
-    storeSetup = [
-        {
-            title: 'hen',
-            price: 100,
-            amount: 0
-        },
-        {
-            title: 'fox',
-            price: 777,
-            amount: 0
-        },
-        {
-            title: 'grain',
-            price: 15,
-            amount: 100
-        }
-    ];
+  AddProductItem = Backbone.View.extend({
+    el: '#addProductItem',
+    events: {
+      'submit': 'submit'
+    },
 
-    views = [];
+    submit: function (e) {
+      e.preventDefault();
+      var title = this.$el.find('input[name=title]').val();
+      var productivity = parseFloat(this.$el.find('input[name=productivity]').val());
+      var consumes = this.$el.find('input[name=consumes]').val();
+      var price = parseInt(this.$el.find('input[name=price]').val());
 
-    for (item in farmSetup) {
-        if (farmSetup.hasOwnProperty(item)) {
-            var model = new Product(farmSetup[item]);
-            myfarm.add(model);
+      if (title && productivity && price) {
+
+        var model = new Product({
+          title: title,
+          productivity: productivity,
+          level: 1,
+          consumes: consumes ? consumes : null
+        });
+        myfarm.add(model);
+
+        var storeModel = new StoreItem({
+          title: title,
+          price: price,
+          amount: 0
+        });
+        myStore.add(storeModel);
+      }
+      // console.log(myfarm, myStore);
+      this.$el[0].reset();
+    }
+  });
+
+  myfarm = new Farm;
+  myStore = new Store;
+  farmSetup = [
+    {
+      title: 'grain',
+      productivity: 5,
+      level: 1,
+      consumes: null
+    },
+    {
+      title: 'hen',
+      productivity: 1,
+      level: 1,
+      consumes: 'grain'
+    },
+    {
+      title: 'fox',
+      productivity: 0.5,
+      level: 1,
+      consumes: 'hen'
+    }
+  ];
+  storeSetup = [
+    {
+      title: 'hen',
+      price: 100,
+      amount: 0
+    },
+    {
+      title: 'fox',
+      price: 777,
+      amount: 0
+    },
+    {
+      title: 'grain',
+      price: 15,
+      amount: 100
+    }
+  ];
+
+  // views = [];
+
+  for (item in farmSetup) {
+    if (farmSetup.hasOwnProperty(item)) {
+      var model = new Product(farmSetup[item]);
+      myfarm.add(model);
+      /*
             views.push(
-                new ProductView({
-                    model: model
-                })
+              new ProductView({
+                model: model
+              })
             )
-        }
+      */
     }
+  }
 
-    for (item in storeSetup) {
-        if (storeSetup.hasOwnProperty(item)) {
-            var storeModel = new StoreItem(storeSetup[item]);
-            myStore.add(storeModel);
-        }
+  for (item in storeSetup) {
+    if (storeSetup.hasOwnProperty(item)) {
+      var storeModel = new StoreItem(storeSetup[item]);
+      myStore.add(storeModel);
     }
-    storeView = new storeViews.Store({collection: myStore, el: $('#store')});
-    storeView.render();
+  }
+  storeView = new storeViews.Store({collection: myStore, el: $('#store')});
+  storeView.render();
 
-    var gameTimer = setInterval(function () {
-        _.each(myfarm.models, function (model) {
-            var level = model.get('level');
-            var productivity = model.get('productivity') * (level>0?Math.log(level+1):0);
-            var complete = model.get('complete');
-            if (complete > 100) {
-                model.trigger('completeProduction');
-            } else {
-                complete = Math.round(100 * (complete + productivity)) / 100;
-                model.set('complete', complete);
-            }
-        })
-    }, 1000);
+  productViews = new ProductViewCollectoin({collection: myfarm});
+
+  addProductItem = new AddProductItem();
+
+  var gameTimer = setInterval(function () {
+    _.each(myfarm.models, function (model) {
+      var level = model.get('level');
+      var productivity = model.get('productivity') * (level > 0 ? Math.log(level + 1) : 0);
+      var complete = model.get('complete');
+      if (Math.random() < 0.001) {
+        // console.log('fire');
+        myStore.where({title: model.get('title')})[0].trigger('fire');
+        return;
+      }
+      if (Math.random() < 0.02 && model.get('title') === 'hen') {
+        // console.log('wildChickens', model.toJSON());
+        myStore.where({title: model.get('title')})[0].trigger('wildChickens');
+      }
+      if (complete > 100) {
+        model.trigger('completeProduction');
+      } else {
+        complete = Math.round(100 * (complete + productivity)) / 100;
+        model.set('complete', complete);
+      }
+    })
+  }, 1000);
 });
